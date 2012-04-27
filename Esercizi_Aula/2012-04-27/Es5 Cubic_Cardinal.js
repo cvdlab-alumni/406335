ed675971baec36d526001413b3490439b98adc56
Claudio Pisanu <newsid88@gmail.com>
2012-04-27 11:04:32 +0200

    esercizi

--- Esercizi_Aula/2012-04-27/Es5 Cubic_Cardinal.js
+++ Esercizi_Aula/2012-04-27/Es5 Cubic_Cardinal.js
@@ -1,4 +1,4 @@
 var domain = INTERVALS(1)(20);
-var controlpoints = [[-3,6],[-4,2],[-3,-1],[-1,1],[1.5,1.5],[3,4],[5,5],[7,2],[6,-2],[2,-3]];
+var controlpoints = [[-3,6,1],[-4,2,3],[-3,-1,-4],[-1,1,2],[1.5,1.5,0],[3,4,8],[5,5,3],[7,2,1],[6,-2,-1],[2,-3,2]];
 var splineCardinal = SPLINE(CUBIC_CARDINAL(domain))(controlpoints);
 DRAW(splineCardinal);
